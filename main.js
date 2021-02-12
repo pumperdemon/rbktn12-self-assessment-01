41781fcbb07b961f5ce1d1d1040c0f2dcaf3fb85
Raed Fadhlaoui <fadhlaouiraied@hotmail.com>
2021-02-12 21:45:19 +0100

    init

---
+++ main.js
@@ -0,0 +1,37 @@
+// -------------------------------------------------------------------------------------------------
+// 1 - Complete the function cube that returns the cube of x:
+function cube(x) {
+    // your code here
+}
+
+// -------------------------------------------------------------------------------------------------
+// 2 - Write a function rentalCar that accepts a person's name and age as arguments,
+// and returns either 'You cannot have the keys, .', or 'Have fun driving, !'
+// depending on whether or not the person is old enough.
+// In the US, most rental car companies do not allow you to rent a car until you are 21.
+// your code here
+
+// -------------------------------------------------------------------------------------------------
+// 3 - You should have noticed a large amount of repetitive code when modifying
+// scoreToGrade to accommodate + or - grades. When we do lots of repetitive things,
+// that's a clear signal that there is a better way. Write a helper function letterGrade
+// that accepts two arguments, letter and score, and works like this:
+
+function letterGrade(letter, score) {
+    // your code here
+}
+// These are examples of what a *working* function would output.
+// letterGrade("A", 95); // => 'A'
+// letterGrade("A", 91); // => 'A-'
+// letterGrade("B", 88); // => 'B+'
+// letterGrade("monkey", 160); // => 'monkey-'
+
+// -------------------------------------------------------------------------------------------------
+// 4 - Write a function called counter that, when invoked, always returns a number that
+// is one more than the previous invocation. For instance:
+function counter() {
+    // TODO: your code here
+}
+// counter(); // => 1
+// counter(); // => 2
+// counter(); // => 3
